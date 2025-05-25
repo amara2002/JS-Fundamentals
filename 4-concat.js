d1aa6fedc5dfaa419de047d2585be926951d3f74
a7e12c6f0d8f <nyeiamara1@gmail.com>
2025-05-25 12:13:58 +0300

    Add script to print message based on argument count

--- 4-concat.js
+++ 4-concat.js
@@ -1,7 +1,3 @@
-    const args = process.argv.slice(2);
+const args = process.argv.slice(2);
 
-    if (args[0] === undefined || args[1] === undefined) {
-    console.log("Please provide two arguments");
-    } else {
-    console.log(`${args[0]} is ${args[1]}`);
-    }
+console.log(`${args[0]} is ${args[1]}`);
