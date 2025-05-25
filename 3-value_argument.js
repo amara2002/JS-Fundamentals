fd2b0494e7a65c0862e639ce1230e3a7cd979284
a7e12c6f0d8f <nyeiamara1@gmail.com>
2025-05-25 12:07:02 +0300

    Create argument count checker script

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -3,7 +3,10 @@
     if (args[0] === undefined) {
     console.log("No argument");
     } else if (args[1] === undefined) {
-    console.log("Argument found");
+    console.log(`one argument "${args[0]}"`);
+    } else if (args[2] === undefined) {
+    console.log(`2 arguments: "${args[0]} ${args[1]}"`);
     } else {
     console.log("Arguments found");
     }
+
