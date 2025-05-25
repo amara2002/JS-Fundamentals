c139fdb0ea934770fc16c206d418055f8a745eab
a7e12c6f0d8f <nyeiamara1@gmail.com>
2025-05-25 12:56:04 +0300

    Printing missing number of occutrences

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,12 +1,13 @@
     const args = process.argv.slice(2);
     const x = parseInt(args[0], 10);
 
-    if (isNaN(x) || x <= 0) {
+    if (isNaN(x)) {
     console.log("Missing number of occurrences");
-    } else {
+    } else if (x > 0) {
     let i = 0;
     while (i < x) {
         console.log("C is fun");
         i++;
     }
     }
+    // else for x <= 0 (like -3), print nothing
