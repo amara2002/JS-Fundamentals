3082cc82d5c06a6c178aa6865b1fd6a58d293947
a7e12c6f0d8f <nyeiamara1@gmail.com>
2025-05-25 13:02:01 +0300

    Project 9

---
+++ 9-add.js
@@ -0,0 +1,9 @@
+    function add(a, b) {
+    return a + b;
+    }
+
+    const args = process.argv.slice(2);
+    const a = parseInt(args[0], 10);
+    const b = parseInt(args[1], 10);
+
+    console.log(add(a, b));
